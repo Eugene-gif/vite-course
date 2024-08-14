e8c7597c59be153015afdcc7915aa8ff153b9e11
Eugene <zuevevgen93@mail.ru>
2024-08-14 14:38:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// import './src/02.js';
+// import './src/assets/03.css';
+// import { heading as headingFontSize } from './src/assets/04a.module.css';
+// import { heading as headingColor } from './src/assets/04b.module.css';
+
+// console.log(headingFontSize, headingColor);
+
+// document.querySelector('.heading').classList.add(headingFontSize, headingColor);
+
+import './src/assets/05.css';
+
---
+++ src/02.js
@@ -0,0 +1,4 @@
+import collect from 'collect.js';
+
+console.log('module a');
+console.log(collect([1, 3, 3, 7]).avg());
