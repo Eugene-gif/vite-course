4717f049418c028f4e22db463062ab5f1c96b0ea
Eugene <zuevevgen93@mail.ru>
2024-08-22 18:08:45 +0300

    Оптимизация картинок

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 import './main.scss'
+import image2 from './image_2.jpeg'
+
+document.querySelector('img').src = image2;
 
 console.log(import.meta.env);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 // vite.config.ts
-import Inspect from 'vite-plugin-inspect'
-import checker from 'vite-plugin-checker'
-import path, { resolve } from 'path'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import Inspect from 'vite-plugin-inspect';
+import checker from 'vite-plugin-checker';
+import path, { resolve } from 'path';
 
 export default {
   // publicDir: 'build', // по умолчанию это папка public
@@ -14,6 +15,14 @@ export default {
     checker({
       typescript: true,
     }),
+    ViteImageOptimizer({
+      jpeg: {
+        quality: 40,
+      },
+      jpg: {
+        quality: 40,
+      },
+    })
   ],
   envPrefix: 'APP_',
   resolve: {
